dded4455aa6e608b8b75c627f1a8881164dcecb7
shr-98 <sharmashrey0004@gmail.com>
2026-04-29 20:57:01 +0530

    update revision for index.html in service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5lvdoul88oo"
+    "revision": "0.a65jied2ct8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
