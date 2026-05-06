5de0359c7b7a2c9dd9b8983ce3947daf7f991b99
uzair <uzairtagala120@gmail.com>
2026-05-06 18:37:13 +0530

    setup bg change

--- src/pages/Setup.tsx
+++ src/pages/Setup.tsx
@@ -187,7 +187,7 @@ const Setup = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-100 p-4 sm:p-6 lg:p-8">
+    <div className="min-h-screen bg-[#FAF7F3]  sm:p-6 lg:p-8">
       <div className="max-w-6xl mx-auto">
         {/* Header */}
         <div className="mb-8">
